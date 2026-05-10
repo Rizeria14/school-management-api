917fcbcd1365b586e0a471c30d65b40e61232c80
Atharva K <cricatharva@gmail.com>
2026-05-10 19:15:21 +0530

    db.js debug

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,12 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 const testConnection = async () => {
   try {
